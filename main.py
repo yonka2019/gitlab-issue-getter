3483df6549e8f75c926b4b67846e7f8c631ed698
Yonka <yonka2003@gmail.com>
2022-12-08 20:34:36 +0200

    up

---
+++ main.py
@@ -0,0 +1,29 @@
+import gitlab
+import datetime
+
+FROM_DATE = (datetime.datetime.now() - datetime.timedelta(days=7))  # - 7 days from now
+TO_DATE = (datetime.datetime.now() + datetime.timedelta(days=7))  # + 7 days from now
+
+assignees = {
+    "EyalSol": "איל",
+    "yonka2019": "יונתן"
+}
+
+gl = gitlab.Gitlab(private_token='glpat-MsKuF3VDgXm3NXbcSrtn')
+issues = gl.issues.list(get_all=True)
+project = gl.projects.get(39312664)
+
+printed = False
+
+print("-- TO-DO -- ")
+for issue in project.issues.list(get_all=True, order_by='due_date'):
+    if issue.due_date is not None:
+        due_date = datetime.datetime.strptime(issue.due_date, '%Y-%m-%d')
+        if FROM_DATE <= due_date <= TO_DATE:
+
+            if (due_date <= datetime.datetime.now()) and not printed:  # print only one time
+                print("-- DONE --")
+                printed = True
+
+            print(issue.title + " [" + due_date.strftime("%d.%m") + "] - " + assignees[issue.assignee["username"]] + " :אחראי ")
+
