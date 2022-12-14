f4c664e2f48ff1cf39381af45b9ecd6e4756d771
Yonka <yonka2003@gmail.com>
2022-12-14 23:38:16 +0200

    up

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 import gitlab
 import datetime
 
-FROM_DATE = (datetime.datetime.now() - datetime.timedelta(days=7))  # - 7 days from now
-TO_DATE = (datetime.datetime.now() + datetime.timedelta(days=7))  # + 7 days from now
 
-assignees = {
-    "EyalSol": "איל",
-    "yonka2019": "יונתן"
-}
+def main():
+    lesson_date = next_weekday(datetime.datetime.now(), 3)  # 0 - monday
 
-gl = gitlab.Gitlab(private_token='token')
-issues = gl.issues.list(get_all=True)
-project = gl.projects.get(project_id)
+    from_date = (lesson_date - datetime.timedelta(days=7))  # - 7 days from now
+    to_date = (lesson_date + datetime.timedelta(days=7))  # + 7 days from now
 
-printed = False
+    print(f"\n[i] FROM DATE: {from_date.date()} -> [{lesson_date.date()}] -> {to_date.date()}\n")
 
-print("-- TO-DO -- ")
-for issue in project.issues.list(get_all=True, order_by='due_date'):
-    if issue.due_date is not None:
-        due_date = datetime.datetime.strptime(issue.due_date, '%Y-%m-%d')
-        if FROM_DATE <= due_date <= TO_DATE:
+    assignees = {
+        "EyalSol": "איל",
+        "yonka2019": "יונתן"
+    }
 
-            if (due_date <= datetime.datetime.now()) and not printed:  # print only one time
-                print("-- DONE --")
-                printed = True
+    gl = gitlab.Gitlab(private_token='glpat-MsKuF3VDgXm3NXbcSrtn')
+    project = gl.projects.get(39312664)
 
-            print(issue.title + " [" + due_date.strftime("%d.%m") + "] - " + assignees[issue.assignee["username"]] + " :אחראי ")
+    printed = False
 
+    print("-- TO-DO -- ")
+    for issue in project.issues.list(get_all=True, order_by='due_date'):
+        if issue.due_date is not None:
+            due_date = datetime.datetime.strptime(issue.due_date, '%Y-%m-%d')
+
+            if from_date <= due_date <= to_date:
+                if (due_date <= lesson_date) and not printed:  # print only one time
+                    print("-- DONE --")
+                    printed = True
+
+                print(issue.title + " [" + due_date.strftime("%d.%m") + "] - " + assignees[issue.assignee["username"]] + " :אחראי ")
+
+
+def next_weekday(date, day):
+    """
+    Returns the date of the next given weekday after
+    the given date. For example, the date of next Monday.
+
+    NB: if it IS the day we're looking for, this returns 0.
+    consider then doing onDay(foo, day + 1).
+    """
+    days = (day - date.weekday() + 7) % 7
+    return date + datetime.timedelta(days=days)
+
+if __name__ == '__main__':
+    main()
